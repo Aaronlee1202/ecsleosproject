481721367eb3b525a0a7cbab4fad3a92f8e5e12e
AaronLee <aaronlee120213@gmail.com>
2023-10-22 18:51:29 +0800

    gltf to flb

--- src/components/cubesat/PlanetModule.vue
+++ src/components/cubesat/PlanetModule.vue
@@ -38,10 +38,10 @@ onBeforeUnmount(() => {
 function initScene() {
   scene = new Scene();
 
-  let Plant = '/src/assets/three/planet.gltf';
-  if (import.meta.env.MODE != 'development') {
-    Plant = `${import.meta.env.BASE_URL}src/assets/three/planet.gltf`;
-  }
+  let Plant = '/src/assets/three/planet.glb';
+  // if (import.meta.env.MODE != 'development') {
+  //   Plant = `${import.meta.env.BASE_URL}src/assets/three/planet.gltf`;
+  // }
   // loadGLTF
   loader.load(
     // resource URL
